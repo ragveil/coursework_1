fc948394af1a69f5046c342aee19da5e9cea6b1f
Nadezhda Kornienko <service911@bk.ru>
2025-06-14 21:13:39 +1000

    feat: add spending_by_category function to reports.py module

--- src/reports.py
+++ src/reports.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime, timedelta
+
 import pandas as pd
 
 from src.utils import path
@@ -16,4 +18,26 @@ def get_dataframe() -> pd.DataFrame:
     df = df[df["Сумма платежа"] < 0]
     return df
 
+
+
+# Функция возвращает траты по заданной категории за последние три месяца (от переданной даты).
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None):
+    if date:
+        end_date = datetime.strptime(date, "%d.%m.%Y").date()
+    else:
+        end_date = datetime.today().date()
+    period = timedelta(days=90)
+    start_date = end_date - period
+    mask = (transactions["Дата платежа"] >= start_date) & (transactions["Дата платежа"] <= end_date)
+    df = transactions.loc[mask]
+    try:
+        df = df[df['Категория'] == category]
+    except KeyError:
+        return 'Неверно введена категория'
+    else:
+        df['Дата платежа'] = pd.to_datetime(df['Дата платежа']).dt.strftime('%d.%m.%Y')
+        return df.to_dict(orient='records')
+
+
 data_frame = get_dataframe()
+print(spending_by_category(data_frame, 'Супермаркеты', '08.05.2019'))
