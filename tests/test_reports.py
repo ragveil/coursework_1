f377ecbc4d89559ed5e05ad47df34dad7a350dd9
Nadezhda Kornienko <service911@bk.ru>
2025-06-18 01:51:01 +1000

    tests: update tests

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,77 +1,91 @@
-import json
-import os
 from datetime import datetime
-from typing import Any
 
+import freezegun
 import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
-import freezegun
-from config import ROOT_DIR
+
+from src.constants import DF_FOR_TESTS, FILTERED_DF_FOR_TESTS, PATH_TO_LOGS, TINY_DF_FOR_TESTS
 from src.reports import get_dataframe, save_to_file, spending_by_category, spending_by_weekday, spending_by_workday
 
-path_to_logs = os.path.join(ROOT_DIR, "logs/")
-path_to_data = os.path.join(ROOT_DIR, "data/")
-path_to_test_df_tiny = os.path.join(path_to_data, "test_operations_2.xlsx")
-path_to_test_df = os.path.join(path_to_data, "test_operations_1.xlsx")
-path_to_filtered_df = os.path.join(path_to_data, "test_filtered_data.xlsx")
+path_to_logs = PATH_TO_LOGS
+path_to_test_df_tiny = TINY_DF_FOR_TESTS
+path_to_test_df = DF_FOR_TESTS
+path_to_filtered_df = FILTERED_DF_FOR_TESTS
+
 
 def test_get_data_frame_correct() -> None:
     result_df = get_dataframe(path_to_test_df_tiny)
-    date = datetime.strptime('01.01.2018', "%d.%m.%Y").date()
-    expected_df = pd.DataFrame({'Дата платежа': [date, date], 'Сумма платежа': [-316, -3000], 'Категория': ['Красота', 'Переводы']})
+    date = datetime.strptime("01.01.2018", "%d.%m.%Y").date()
+    expected_df = pd.DataFrame(
+        {"Дата платежа": [date, date], "Сумма платежа": [-316, -3000], "Категория": ["Красота", "Переводы"]}
+    )
     assert_frame_equal(result_df, expected_df)
 
-def test_form_data_frame_no_file():
-    with pytest.raises(FileNotFoundError):
-        assert get_dataframe('no_file_path')
 
-def test_spending_by_category_correct(spending_by_category_correct) -> None:
+def test_form_data_frame_no_file() -> None:
+    with pytest.raises(FileNotFoundError):
+        assert get_dataframe("no_file_path")
+
+
+def test_spending_by_category_correct(spending_by_category_correct: list[dict]) -> None:
     df = get_dataframe(path_to_test_df)
-    assert spending_by_category(df, 'Супермаркеты', '31.12.2021') == spending_by_category_correct
+    assert spending_by_category(df, "Супермаркеты", "31.12.2021") == spending_by_category_correct
+
 
 @freezegun.freeze_time("2021-12-31")
-def test_spending_by_category_today(spending_by_category_correct) -> None:
+def test_spending_by_category_today(spending_by_category_correct: list[dict]) -> None:
     df = get_dataframe(path_to_test_df)
-    assert spending_by_category(df, 'Супермаркеты') == spending_by_category_correct
+    assert spending_by_category(df, "Супермаркеты") == spending_by_category_correct
 
-@pytest.mark.parametrize('category, date, expected', [('ЖКХ', '31.12.2021', []), ('Супермаркет', '31.12.2222', [])])
-def test_spending_by_category_incorrect(category, date, expected) -> None:
+
+@pytest.mark.parametrize("category, date, expected", [("ЖКХ", "31.12.2021", []), ("Супермаркет", "31.12.2222", [])])
+def test_spending_by_category_incorrect(category: str, date: str, expected: list) -> None:
     df = get_dataframe(path_to_test_df)
     assert spending_by_category(df, category, date) == expected
 
-def test_spending_by_weekday_correct(spending_by_weekday_correct) -> None:
+
+def test_spending_by_weekday_correct(spending_by_weekday_correct: dict) -> None:
     df = get_dataframe(path_to_filtered_df)
-    assert spending_by_weekday(df, '31.12.2021') == spending_by_weekday_correct
+    assert spending_by_weekday(df, "31.12.2021") == spending_by_weekday_correct
+
 
 @freezegun.freeze_time("2021-12-31")
-def test_spending_by_weekday_today(spending_by_weekday_correct) -> None:
+def test_spending_by_weekday_today(spending_by_weekday_correct: dict) -> None:
     df = get_dataframe(path_to_filtered_df)
     assert spending_by_weekday(df) == spending_by_weekday_correct
 
-def test_spending_by_workday_correct(spending_by_workday_correct) -> None:
+
+def test_spending_by_workday_correct(spending_by_workday_correct: dict) -> None:
     df = get_dataframe(path_to_filtered_df)
-    assert spending_by_workday(df, '31.12.2021') == spending_by_workday_correct
+    assert spending_by_workday(df, "31.12.2021") == spending_by_workday_correct
+
 
 @freezegun.freeze_time("2021-12-31")
-def test_spending_by_workday_today(spending_by_workday_correct) -> None:
+def test_spending_by_workday_today(spending_by_workday_correct: dict) -> None:
     df = get_dataframe(path_to_filtered_df)
     assert spending_by_workday(df) == spending_by_workday_correct
 
-def test_summ_num_filename(capsys: Any) -> None:
-    @save_to_file('some_file.json')
-    def summ_num(a: int, b: int) -> int:
-        return a + b
-    summ_num(1, 2)
-    with open(path_to_logs + "some_file.json", "r", encoding="UTF-8", newline="\n") as f:
-        log_file = json.load(f)
-        assert 3 == log_file
 
-def test_summ_num_default(capsys: Any) -> None:
+def test_zero_division_filename(zero_division_decor: str) -> None:
+    @save_to_file("some_file.json")
+    def zero_division(a: int, b: int) -> float:
+        return a / b
+
+    zero_division(4, 2)
+    zero_division(4, 0)
+    with open(path_to_logs + "some_file.json", "r", encoding="UTF-8", newline="\n") as f:
+        log_file = f.read()
+        assert zero_division_decor == log_file
+
+
+def test_zero_division_default(zero_division_decor: str) -> None:
     @save_to_file()
-    def summ_num(a: int, b: int) -> int:
-        return a + b
-    summ_num(4, 3)
+    def zero_division(a: int, b: int) -> float:
+        return a / b
+
+    zero_division(4, 2)
+    zero_division(4, 0)
     with open(path_to_logs + "report.json", "r", encoding="UTF-8", newline="\n") as f:
-        log_file = json.load(f)
-        assert 7 == log_file
+        log_file = f.read()
+        assert zero_division_decor == log_file
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,54 +1,66 @@
-
-import os
 from datetime import datetime
+from typing import Any
 
 import pandas as pd
 import pytest
-import requests
 from pandas.testing import assert_frame_equal
 from pytest_mock import MockFixture
+from requests import ConnectionError
 
-from config import ROOT_DIR
+from src.constants import DF_FOR_TESTS, PATH_TO_DATA, TINY_DF_FOR_TESTS
 from src.utils import form_data_frame, get_currency_rates, get_expenses, get_income, get_period
 
-path_to_data = os.path.join(ROOT_DIR, "data")
-path_to_test_df_tiny = os.path.join(path_to_data, "test_operations_2.xlsx")
-path_to_test_df = os.path.join(path_to_data, "test_operations_1.xlsx")
+path_to_data = PATH_TO_DATA
+path_to_test_df_tiny = TINY_DF_FOR_TESTS
+path_to_test_df = DF_FOR_TESTS
 
 
 def test_form_data_frame_correct() -> None:
     result_df = form_data_frame(path_to_test_df_tiny)
-    date_1 = datetime.strptime('04.01.2018', "%d.%m.%Y").date()
-    date_2 = datetime.strptime('01.01.2018', "%d.%m.%Y").date()
-    expected_df = pd.DataFrame({'date': [date_1, date_2], 'amount': [-316, -3000], 'category': ['Красота', 'Переводы']})
+    date_1 = datetime.strptime("01.01.2018", "%d.%m.%Y").date()
+    date_2 = datetime.strptime("01.01.2018", "%d.%m.%Y").date()
+    expected_df = pd.DataFrame(
+        {"date": [date_1, date_2], "amount": [-316, -3000], "category": ["Красота", "Переводы"]}
+    )
     assert_frame_equal(result_df, expected_df)
 
 
-
-def test_form_data_frame_no_file():
+def test_form_data_frame_no_file() -> None:
     with pytest.raises(FileNotFoundError):
-        assert form_data_frame('no_file_path')
+        assert form_data_frame("no_file_path")
 
-@pytest.mark.parametrize('date, period', [('31.12.2021', 'Y'), ('31.12.2021', 'W'), ('31.12.2021', 'D'), ('31.12.2021', 'M'), ('31.12.2021', 'ALL')])
-def test_get_period_correct(date, period):
+
+@pytest.mark.parametrize(
+    "date, period",
+    [("31.12.2021", "Y"), ("31.12.2021", "W"), ("31.12.2021", "D"), ("31.12.2021", "M"), ("31.12.2021", "ALL")],
+)
+def test_get_period_correct(date: str, period: str) -> None:
     result_df = get_period(date, period).head(1)
-    date = datetime.strptime('31.12.2021', "%d.%m.%Y").date()
-    expected_df = pd.DataFrame({'date': [date], 'amount': [-160.89], 'category': ['Супермаркеты']})
+    date_ = datetime.strptime("31.12.2021", "%d.%m.%Y").date()
+    expected_df = pd.DataFrame({"date": [date_], "amount": [-160.89], "category": ["Супермаркеты"]})
     assert_frame_equal(result_df, expected_df)
 
-def test_get_period_incorrect():
-    result_df = get_period('31.12.2035').head(1)
+
+def test_get_period_incorrect() -> None:
+    result_df = get_period("31.12.2035").head(1)
     assert result_df.empty
 
-def test_get_expenses_correct():
+
+def test_get_expenses_correct() -> None:
     df = form_data_frame(path_to_test_df_tiny)
-    assert get_expenses(df) == {'main': [{'Другое': 0, 'Красота': -316}], 'total_amount': -3316, 'transfers_and_cash': [{'Переводы': -3000}]}
+    assert get_expenses(df) == {
+        "main": [{"Другое": 0, "Красота": -316}],
+        "total_amount": -3316,
+        "transfers_and_cash": [{"Переводы": -3000}],
+    }
 
-def test_get_income_correct():
+
+def test_get_income_correct() -> None:
     df = form_data_frame(path_to_test_df)
-    assert get_income(df) == {'main': [{'Пополнения': 179046}], 'total_amount': 179046}
+    assert get_income(df) == {"main": [{"Пополнения": 179046}], "total_amount": 179046}
 
-def test_get_currency_rates_correct(mocker: MockFixture, mock_currency_response) -> None:
+
+def test_get_currency_rates_correct(mocker: MockFixture, mock_currency_response: list[dict]) -> None:
     mock_response = mocker.patch("requests.get")
     mock_response.return_value.json.return_value = {
         "success": True,
@@ -61,14 +73,17 @@ def test_get_currency_rates_correct(mocker: MockFixture, mock_currency_response)
     mock_response.assert_called()
 
 
-def test_get_currency_rates_error(mocker: MockFixture):
-    mock_response = mocker.patch("src.utils.requests.get", side_effect=requests.exceptions.ConnectionError)
+def test_get_currency_rates_error(mocker: MockFixture) -> None:
+    mock_response = mocker.patch("src.utils.requests.get", side_effect=ConnectionError)
     assert get_currency_rates() == "Ошибка обращения к api"
     mock_response.assert_called_once()
 
-def test_get_stock_rates(monkeypatch, mock_stock_response):
-    def mock_get_stock_rates():
+
+def test_get_stock_rates(monkeypatch: Any, mock_stock_response: list[dict]) -> None:
+    def mock_get_stock_rates() -> Any:
         return mock_stock_response
-    monkeypatch.setattr('src.utils.get_stock_rates', mock_get_stock_rates)
+
+    monkeypatch.setattr("src.utils.get_stock_rates", mock_get_stock_rates)
     from src.utils import get_stock_rates
+
     assert get_stock_rates() == mock_stock_response
